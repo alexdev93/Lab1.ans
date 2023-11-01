1ac30877f027e1b345f6bc047a996db09fc47e13
afteralexxo <itsalextwod@gmail.com>
2023-10-31 22:08:43 -0700

    remove and modify exceptions

--- FriendList.java
+++ FriendList.java
@@ -21,38 +21,27 @@ class FriendList {
 
     public void addFriend() {
         Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.print("Enter your friend's name: ");
-            String friendName = scanner.nextLine();
-            friends.add(friendName);
-            saveFriendsList();
-            System.out.println(friendName + " added to your friend list.");
-        } finally {
-            // scanner.close();
-        }
+        System.out.print("Enter your friend's name: ");
+        String friendName = scanner.nextLine();
+        friends.add(friendName);
+        saveFriendsList();
+        System.out.println(friendName + " added to your friend list.");
     }
 
     public void readFriendsList() {
-        try  {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName))
-            String line;
-            while ((line = reader.readLine()) != null) {
-                friends.add(line);
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading from " + fileName);
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            friends.add(line);
+            System.out.println(line);
         }
     }
 
     private void saveFriendsList() {
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter("friends.list"))
-            for (String friend : friends) {
-                writer.println(friend);
-            }
-        } catch (IOException e) {
-            System.err.println("Error writing to friends.list");
+        FileWriter fileWriter = new FileWriter("friends.list");
+        PrintWriter writer = new PrintWriter(fileWriter);
+        for (String friend : friends) {
+            writer.println(friend);
         }
     }
 }
