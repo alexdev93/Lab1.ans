420c7872eea26529eb85ff8e1c4d951fbe101b1f
afteralexxo <itsalextwod@gmail.com>
2023-10-31 22:07:47 -0700

    remove the try catch exceptions

--- Chat.java
+++ Chat.java
@@ -21,40 +21,29 @@ class Chat implements ChatOperations {
 
     @Override
     public void readChat() {
-        try {
+        
             BufferedReader reader = new BufferedReader(new FileReader(fileName))
             String line;
             while ((line = reader.readLine()) != null) {
                 chat.add(line);
                 System.out.println(line);
             }
-        } catch (IOException e) {
-            System.err.println("Error reading from " + fileName);
-        }
     }
 
     @Override
     public void writeChat() {
         Scanner scanner = new Scanner(System.in);
-        try {
             System.out.print("Enter your message: ");
             String message = scanner.nextLine();
             chat.add(message);
             saveChat();
             System.out.println("Message added to the chat.");
-        } finally {
-            // scanner.close();
-        }
     }
 
     public void saveChat() {
-        try {
             PrintWriter writer = new PrintWriter(new FileWriter(fileName))
             for (String message : chat) {
                 writer.println(message);
             }
-        } catch (IOException e) {
-            System.err.println("Error writing to " + fileName);
-        }
     }
 }
